d515847a0ff347ae434e0c52201766d50e97c134
Fredrik Sundström <sumpe@DESKTOP-FGIPE4A.HEATIA1.local>
2022-10-01 13:19:42 +0200

    221001

--- src/StartUp.java
+++ src/StartUp.java
@@ -9,7 +9,7 @@ import helper.io.IOHandler;
 
 import static helper.methods.CommonMethods.buildPolygonShape;
 import static helper.methods.CommonMethods.intArrToPointArr;
-//https://github.com/OneLoneCoder/olcPixelGameEngine/blob/master/olcPixelGameEngine.h
+
 
 public class StartUp {
 
@@ -20,17 +20,10 @@ public class StartUp {
         IOHandler.removeFilesFromFolder("./resources/files/log/gc");
         IOHandler.removeFile("./resources/files/log/error/error.log");
 
-        /*int[] arr = new int[]{10,10,50,50,80,30,100,20};
 
-        Point[] points = intArrToPointArr(arr);
-        points = buildPolygonShape(points);
-        for(int i = 0;i<points.length;i++){
-            IOHandler.printPoint(points[i]);
-        }*/
-
-        //program = new SpellMe(800,500);
+        program = new SpellMe(800,500);
         //program = new ColorPicker(800,500);
-        program = new QuadTreeBalls(800,500);
+        //program = new QuadTreeBalls(800,500);
         if(program.setUpProgram()){
             program.runEngineLoop();
         }
