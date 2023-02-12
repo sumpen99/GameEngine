74a1b2759530107258bc0d941719bf6db4048cbf
Fredrik Sundström <sumpe@DESKTOP-FGIPE4A.HEATIA1.local>
2023-02-12 15:48:29 +0100

    230212

--- resources/files/gui/imagesampling.fs
+++ resources/files/gui/imagesampling.fs
@@ -4,3 +4,29 @@
 # id:string
 # bind:(idToWidgetToBindTo)
 # args:(idToWidgetToBindTo),(idToWidgetToBindTo),...
+HORIZONTAL
+;
+BoxLayout
+    id:layout0
+    left:0
+    top:0
+    width:800
+    height:500
+    valign:false
+    color:LightGreen
+    draw:fill
+    opacity:0
+    shape:rectangle
+;
+FlatButton
+        left:0
+        top:0
+        width:80
+        height:40
+        color:PLUM
+        textcolor:black
+        text:Exit
+        func:shutdown
+        draw:fill
+        shape:rectangle
+;
